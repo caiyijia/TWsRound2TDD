0d2437473ea3dbd33e531527701683d9e1921367
Adam Cai <caiyijias@outlook.com>
2019-05-28 23:17:08 +0800

    finished Round2TDD

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    let parkFee = 0.25 * inputs.parkTime;
+    let price = 6;
+    if (inputs.distance < 2) {
+        price += parkFee;
+    }else if(inputs.distance <= 8) {
+        price += parkFee + 0.8 * (inputs.distance - 2);
+    }else{
+        price += parkFee + 4.8 + 0.8 * 1.5 *(inputs.distance - 8);
+    }
+    return Math.round(price);
 };
